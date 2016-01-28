ffbcfab965e641434ad79a8f6c8ae48acb844074
sid <sidsinh2014@gmail.com>
2016-01-28 15:31:38 +0530

    Modify MainActivity to fix button handling

--- app/src/main/java/com/mycompany/myappportfolio/MainActivity.java
+++ app/src/main/java/com/mycompany/myappportfolio/MainActivity.java
@@ -1,16 +1,30 @@
 package com.mycompany.myappportfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button b1,b2,b3,b4,b5,b6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button1);
+        b1.setOnClickListener(this);
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button) findViewById(R.id.button3);
+        b4 = (Button) findViewById(R.id.button4);
+        b5 = (Button) findViewById(R.id.button5);
+        b6 = (Button) findViewById(R.id.button6);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
     }
 
     @Override
